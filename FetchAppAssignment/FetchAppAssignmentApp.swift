2b3577e2ebaa2a59d8dcf16d11756b0946ba6993
brooklyngibbs <brooklyngibbs22@gmail.com>
2025-02-03 17:45:40 -0600

    Initial Commit

---
+++ FetchAppAssignment/FetchAppAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchAppAssignmentApp.swift
+//  FetchAppAssignment
+//
+//  Created by Brooklyn Gibbs on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchAppAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
